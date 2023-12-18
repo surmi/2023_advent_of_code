35951210c6b485241c0b01f80660e58e288ffd92
MSu <surma.mate@gmail.com>
2023-12-19 00:20:26 +0100

    setup for day 1

---
+++ app.js
@@ -0,0 +1,18 @@
+const process = require('node:process');
+const fs = require('fs');
+
+console.log("Advent of Code 2023")
+
+let daysDir = new Array();
+fs.readdirSync('.').forEach(file => {
+    if (file.includes('day')) {
+        daysDir.push(file);
+    }
+})
+daysDir.sort()
+
+if (process.argv.length === 2) {
+    const recent = daysDir[daysDir.length-1];
+    const d = require(`./${recent}/day`);
+    d.run();
+}
---
+++ day1/day.js
@@ -0,0 +1,7 @@
+function run() {
+    const msg = 'Day 1';
+    console.log(msg);
+}
+
+exports.run = run;
+
