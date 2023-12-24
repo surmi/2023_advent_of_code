c182bb1aaef03fc4480a25e00031f92ea4c50752
MSu <surma.mate@gmail.com>
2023-12-24 23:37:50 +0100

    setup for TS

--- day3/day.js
+++ day3/day.js
@@ -1,13 +1,11 @@
-function run1star(inPath){
-    console.log('Day 3*');
-
-    let lines;
-    try {
-        lines.require('fs').readFileSync(inPath, 'utf-8').split(\n);
-    }
-    catch (err){
-        console.log(err);
-    }
-
-    
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run2star = exports.run1star = void 0;
+function run1star(inPath) {
+    console.log("Day 3*");
 }
+exports.run1star = run1star;
+function run2star(inPath) {
+    console.log("Day3**");
+}
+exports.run2star = run2star;
---
+++ day3/day.ts
@@ -0,0 +1,7 @@
+export function run1star(inPath:string){
+    console.log("Day 3*");
+}
+
+export function run2star(inPath:string){
+    console.log("Day3**");
+}
