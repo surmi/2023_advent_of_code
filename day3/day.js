0d96f25c304bb4a708c7c0e34331b67810055c8a
MSu <surma.mate@gmail.com>
2023-12-24 21:49:16 +0100

    day3 setup

---
+++ day3/day.js
@@ -0,0 +1,13 @@
+function run1star(inPath){
+    console.log('Day 3*');
+
+    let lines;
+    try {
+        lines.require('fs').readFileSync(inPath, 'utf-8').split(\n);
+    }
+    catch (err){
+        console.log(err);
+    }
+
+    
+}
