ac813f487adbd1ab23378625384fe86636edecec
Mateusz Surma <surma.mate@gmail.com>
2024-01-03 16:14:26 +0100

    WiP, changing stations

--- day3/day.js
+++ day3/day.js
@@ -1,8 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run2star = exports.run1star = void 0;
+const fs_1 = require("fs");
 function run1star(inPath) {
     console.log("Day 3*");
+    let lines;
+    try {
+        lines = (0, fs_1.readFileSync)(inPath, 'utf-8').split('\n');
+        console.log(lines.length);
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 exports.run1star = run1star;
 function run2star(inPath) {
--- day3/day.ts
+++ day3/day.ts
@@ -1,5 +1,17 @@
+import { readFileSync } from "fs";
+
 export function run1star(inPath:string){
     console.log("Day 3*");
+
+    let lines;
+    try {
+        lines = readFileSync(inPath, 'utf-8').split('\n');
+
+        console.log(lines.length)
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 export function run2star(inPath:string){
