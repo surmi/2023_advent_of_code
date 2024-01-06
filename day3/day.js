37a14282b8eabe4df61fa0946fc4b86f0b5cb9f1
MSu <surma.mate@gmail.com>
2024-01-06 21:26:10 +0100

    day 3 DONE

--- day3/day.js
+++ day3/day.js
@@ -2,19 +2,143 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run2star = exports.run1star = void 0;
 const fs_1 = require("fs");
+function hitBoxSum(x0, x1, val, boxes) {
+    let sum = 0;
+    for (let b of boxes) {
+        if (b.x0 > x1) {
+            break;
+        }
+        else if (x0 <= b.x1 && b.x0 <= x1) {
+            sum += val;
+        }
+    }
+    return sum;
+}
 function run1star(inPath) {
     console.log("Day 3*");
-    let lines;
-    try {
-        lines = (0, fs_1.readFileSync)(inPath, 'utf-8').split('\n');
-        console.log(lines.length);
-    }
-    catch (err) {
-        console.log(err);
-    }
+    let lines = (0, fs_1.readFileSync)(inPath, 'utf-8').split('\n');
+    lines.pop();
+    let reNums = /\d+/g; // regex for numbers
+    let reSymb = /[^\d\.]/g;
+    // let reSymb = /[^\d\.]/gd; // only for '.indices'
+    let symBoxes = lines.map((line, i) => {
+        //map special signs
+        let boxes = [];
+        let ans;
+        while (null !== (ans = reSymb.exec(line))) {
+            boxes.push({
+                x0: ans.index - 1,
+                // x1:(ans.indices?.at(0)?.at(1))||0, // this method uses feature from ES2021
+                x1: ans.index + 1
+            });
+        }
+        return boxes;
+    });
+    let res = lines.map((line, i) => {
+        // map numbers
+        let lineSum = 0;
+        let prevBoxes = symBoxes[i - 1];
+        let curBoxes = symBoxes[i];
+        let nextBoxes = symBoxes[i + 1];
+        let toCheck = [];
+        if (prevBoxes !== undefined) {
+            toCheck.push(prevBoxes);
+        }
+        toCheck.push(curBoxes);
+        if (nextBoxes !== undefined) {
+            toCheck.push(nextBoxes);
+        }
+        let ans;
+        while (null !== (ans = reNums.exec(line))) {
+            let x0 = ans.index;
+            let x1 = x0 + ans[0].length - 1;
+            for (let boxes of toCheck) {
+                lineSum += hitBoxSum(x0, x1, parseInt(ans[0]), boxes);
+            }
+        }
+        return lineSum;
+    })
+        .reduce((acc, cur) => acc + cur);
+    console.log(res);
 }
 exports.run1star = run1star;
+function hitNum(x0, x1, nums) {
+    let hitNums = [];
+    for (let n of nums) {
+        if (n.x0 > x1) {
+            break;
+        }
+        else if (x0 <= n.x1 && n.x0 <= x1) {
+            hitNums.push(n.val);
+        }
+    }
+    return hitNums;
+}
 function run2star(inPath) {
     console.log("Day 3**");
+    let lines = (0, fs_1.readFileSync)(inPath, 'utf-8').split('\n');
+    lines.pop();
+    // Find all numbers
+    let reNums = /\d+/g; // regex for numbers
+    let reStar = /\*/g; // regex for *
+    let nums = lines.map((line) => {
+        let numArr = [];
+        let matched;
+        while (null !== (matched = reNums.exec(line))) {
+            numArr.push({
+                x0: matched.index,
+                x1: matched.index + matched[0].length - 1,
+                val: parseInt(matched[0])
+            });
+        }
+        return numArr;
+    });
+    console.log(nums[0]);
+    // TODO: find * and check if its box is hit by exactly two numbers; if yes, then multiply both numbers and add to the sum
+    let tmp1 = [];
+    let res = lines.map((line, i) => {
+        let lineSum = 0;
+        let prevNums = nums[i - 1];
+        let curNums = nums[i];
+        let nextNums = nums[i + 1];
+        let toCheck = [];
+        if (prevNums !== undefined) {
+            toCheck.push(prevNums);
+        }
+        toCheck.push(curNums);
+        if (nextNums !== undefined) {
+            toCheck.push(nextNums);
+        }
+        let matched;
+        let tmp = [];
+        while (null !== (matched = reStar.exec(line))) {
+            let x0 = matched.index - 1;
+            let x1 = matched.index + 1;
+            let numsPerStar = [];
+            for (let n of toCheck) {
+                numsPerStar.push(...hitNum(x0, x1, n));
+            }
+            numsPerStar.flat();
+            tmp.push(numsPerStar);
+            if (numsPerStar.length === 2) {
+                lineSum += numsPerStar[0] * numsPerStar[1];
+                tmp1.push(numsPerStar);
+                // writeFileSync("/out.txt", `${numsPerStar[0]}, ${numsPerStar[1]}`);
+            }
+        }
+        // return tmp;
+        return lineSum;
+    }).reduce((acc, cur) => acc + cur);
+    console.log(res);
+    // console.log(tmp1);
+    // for (let t of tmp1){
+    //     console.log(t);
+    // }
+    // for (let st of tmp1)
+    // writeFileSync("/out.txt", tmp1)
+    // for (let i=0; i<10;i++){
+    //     console.log(i+1)
+    //     console.log(res[i]);
+    // }
 }
 exports.run2star = run2star;
--- day3/day.ts
+++ day3/day.ts
@@ -1,19 +1,164 @@
 import { readFileSync } from "fs";
+import { writeFileSync } from "fs";
+
+interface Box{
+    x0: number;
+    x1: number;
+}
+
+function hitBoxSum(x0:number, x1:number, val:number, boxes:Box[]): number{
+    let sum = 0;
+    for (let b of boxes) {
+        if (b.x0 > x1) {
+            break;
+        }
+        else if (x0<=b.x1 && b.x0 <=x1){
+            sum += val;
+        }
+    }
+    return sum;
+}
 
 export function run1star(inPath:string){
     console.log("Day 3*");
 
-    let lines;
-    try {
-        lines = readFileSync(inPath, 'utf-8').split('\n');
+    let lines = readFileSync(inPath, 'utf-8').split('\n');
+    lines.pop();
 
-        console.log(lines.length)
+    let reNums = /\d+/g; // regex for numbers
+    let reSymb = /[^\d\.]/g;
+    // let reSymb = /[^\d\.]/gd; // only for '.indices'
+    let symBoxes = lines.map((line, i) => {
+        //map special signs
+        let boxes: Box[] = [];
+        let ans;
+        while (null !== (ans = reSymb.exec(line))){
+            boxes.push({
+                x0:ans.index-1,
+                // x1:(ans.indices?.at(0)?.at(1))||0, // this method uses feature from ES2021
+                x1:ans.index+1
+            })
+        }
+        return boxes;
+    })
+
+    let res = lines.map((line, i) =>{
+        // map numbers
+        let lineSum = 0;
+        let prevBoxes = symBoxes[i-1];
+        let curBoxes = symBoxes[i];
+        let nextBoxes = symBoxes[i+1];
+
+        let toCheck = [];
+        if (prevBoxes !== undefined) {
+            toCheck.push(prevBoxes);
+        }
+        toCheck.push(curBoxes);
+        if (nextBoxes !== undefined) {
+            toCheck.push(nextBoxes);
+        }
+
+        let ans;
+        while (null !== (ans = reNums.exec(line))){
+            let x0 = ans.index;
+            let x1 = x0+ans[0].length-1;
+            for (let boxes of toCheck){
+                lineSum += hitBoxSum(x0, x1, parseInt(ans[0]), boxes);
+            }
+        }
+        return lineSum
+    })
+    .reduce((acc, cur) => acc + cur);
+    console.log(res);
+}
+
+interface Num{
+    x0:number;
+    x1:number;
+    val:number;
+}
+function hitNum(x0:number, x1:number, nums:Num[]): number[]{
+    let hitNums:number[] = [];
+    for (let n of nums) {
+        if (n.x0 > x1) {
+            break;
+        }
+        else if (x0<=n.x1 && n.x0 <=x1){
+            hitNums.push(n.val);
+        }
     }
-    catch (err) {
-        console.log(err);
-    }
+    return hitNums;
 }
 
 export function run2star(inPath:string){
     console.log("Day 3**");
+
+    let lines = readFileSync(inPath, 'utf-8').split('\n');
+    lines.pop();
+
+    // Find all numbers
+    let reNums = /\d+/g; // regex for numbers
+    let reStar = /\*/g; // regex for *
+    let nums = lines.map((line) =>{
+        let numArr:Num[] = [];
+        let matched;
+        while (null !== (matched = reNums.exec(line))){
+            numArr.push({
+                x0:matched.index,
+                x1:matched.index+matched[0].length-1,
+                val:parseInt(matched[0])
+            })
+        }
+        return numArr;
+    })
+    console.log(nums[0]);
+    // TODO: find * and check if its box is hit by exactly two numbers; if yes, then multiply both numbers and add to the sum
+    let tmp1:number[][] = [];
+    let res = lines.map((line, i)=>{
+        let lineSum = 0;
+        let prevNums = nums[i-1];
+        let curNums = nums[i];
+        let nextNums = nums[i+1];
+
+        let toCheck = [];
+        if (prevNums !== undefined){
+            toCheck.push(prevNums);
+        }
+        toCheck.push(curNums);
+        if (nextNums !== undefined){
+            toCheck.push(nextNums);
+        }
+
+        let matched;
+        let tmp = [];
+        while (null !== (matched = reStar.exec(line))){
+            let x0 = matched.index-1;
+            let x1 = matched.index+1;
+
+            let numsPerStar:number[] = [];
+            for (let n of toCheck) {
+                numsPerStar.push(...hitNum(x0, x1, n));
+            }
+            numsPerStar.flat();
+            tmp.push(numsPerStar);
+            if (numsPerStar.length === 2){
+                lineSum += numsPerStar[0]*numsPerStar[1];
+                tmp1.push(numsPerStar);
+                // writeFileSync("/out.txt", `${numsPerStar[0]}, ${numsPerStar[1]}`);
+            }
+        }
+        // return tmp;
+        return lineSum;
+    }).reduce((acc, cur) => acc + cur);
+    console.log(res);
+    // console.log(tmp1);
+    // for (let t of tmp1){
+    //     console.log(t);
+    // }
+    // for (let st of tmp1)
+    // writeFileSync("/out.txt", tmp1)
+    // for (let i=0; i<10;i++){
+    //     console.log(i+1)
+    //     console.log(res[i]);
+    // }
 }
